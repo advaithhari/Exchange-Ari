581effc4fda9e0a4d49fc4727fa29fdcc4b17d4e
Advaith Hari <33227909+advaithhari@users.noreply.github.com>
2020-08-07 15:35:49 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,12 @@
-
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBeK-RSHNc0Ylf6KyYbmFRh42zEwkemzyg",
+    authDomain: "ari-exchange.firebaseapp.com",
+    databaseURL: "https://ari-exchange.firebaseio.com",
+    projectId: "ari-exchange",
+    storageBucket: "ari-exchange.appspot.com",
+    messagingSenderId: "979167378283",
+    appId: "1:979167378283:web:42433b9a618ed1960b2a2e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
