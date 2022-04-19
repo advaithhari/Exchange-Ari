5e9ff2ec3f6f35647e6ab9590579d0ba28685841
advaithhari <33227909+advaithhari@users.noreply.github.com>
2022-04-19 14:59:34 -0400

    putting everything up to date

--- paypak.js
+++ paypak.js
@@ -1,12 +1,7 @@
 
-// paypal.configure({
-//     'mode': 'sandbox', //sandbox or live
-//     'client_id': 'ASqoNJ95T5DyXjttd8HtJ9fZG6MpI7ZytL8E8zMAH03oDGvoqSNyTj_MLs8gB5wh9fSUfLCaDtkJ9gy9',
-//     'client_secret': 'EO6BSdjpozoAr1LUdTQNDmDrJQ8wO3Rad5GS4SdP3rwxRo4xQRdNWerkMJAxIG_v5chOZC-K6gnGGp3c'
-//   });
 
   paypal.Buttons({
-
+    
     // Sets up the transaction when a payment button is clicked
     createOrder: function(data, actions) {
   return actions.order.create({
@@ -17,7 +12,7 @@
         value: '0.01'
       },
       payee: {
-        email_address: 'ahari-22@peddie.org'
+        email_address: 'advaithhari@gmail.com'
       }
     }]
   });
